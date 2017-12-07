c8bfce47cd37ebe370a5f29a54ae859b3f607b87
chris-dion <michael.c.dion@gmail.com>
2017-12-07 12:48:09 -0600

    skeleton

---
+++ liri.js
@@ -0,0 +1,19 @@
+// Import the Keys file
+var keys = require("./keys.js");
+var Twitter = require('twitter');
+
+var param = process.argv;
+
+var func_call = param [2];
+
+if (func_call === "my-tweets"){
+
+}else if (func_call === "spotify-this-song"){
+
+}else if (func_call === "movie-this"){
+
+}else if (func_call === "do-what-it-says"){
+
+}else{
+
+}
