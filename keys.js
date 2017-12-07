a310f04431c9f40a6fc002103873b5191757172a
chris-dion <michael.c.dion@gmail.com>
2017-12-07 14:36:53 -0600

    songify is working

--- keys.js
+++ keys.js
@@ -4,7 +4,9 @@ var twitterKeys = {
   consumer_key: 'dNzACdUaDubYAIFmPW3O8O7jk',
   consumer_secret: '9ZnaCzKcRzeiaKgs61xE1TIypwZMJHNMYBudp0YiQ7TYoCW3gc',
   access_token_key: '938799138195623936-6gXbh5neJqoHmcQPxr1oEhuCLBByv9t',
-  access_token_secret: '9BFrvoP765a5CyUAoKLZtTS0LTQpykTs7iZMjHkCMfRq6'
+  access_token_secret: '9BFrvoP765a5CyUAoKLZtTS0LTQpykTs7iZMjHkCMfRq6',
+  spot_client:  '6cf4a784704c4725ae1cc180d186de74',
+  spot_secret: '3565907380b8491986848156b562ab51'
 }
 
 module.exports = twitterKeys;
--- liri.js
+++ liri.js
@@ -7,7 +7,16 @@ var client = new Twitter({
 	consumer_secret: keys.consumer_secret,
 	access_token_key: keys.access_token_key,
 	access_token_secret: keys.access_token_secret
-})
+});
+
+var Spotify = require('node-spotify-api');
+ 
+var spotify = new Spotify({
+  id: keys.spot_client,
+  secret: keys.spot_secret
+});
+
+var request = require('request');
 
 var param = process.argv;
 
@@ -26,8 +35,44 @@ if (func_call === "my-tweets"){
 	});
 
 }else if (func_call === "spotify-this-song"){
-
+	if(param.length === 4){
+		var track = param[3];
+	
+		spotify.search({ type: 'track', query: track }, function(err, data) {
+		  if (err) {
+		    return console.log('Error occurred: ' + err);
+		  }
+		console.log("Artist: "+data.tracks.items[0].artists[0].name); 
+		console.log("Album: "+data.tracks.items[0].album.name); 
+		console.log("Song: "+data.tracks.items[0].name); 
+		console.log("preview link : "+data.tracks.items[0].preview_url); 
+		});
+	}else{
+		var track = 'The Sign';
+	
+		spotify.search({ type: 'track', query: track }, function(err, data) {
+		  if (err) {
+		    return console.log('Error occurred: ' + err);
+		  }
+		console.log("Artist: "+data.tracks.items[0].artists[0].name); 
+		console.log("Album: "+data.tracks.items[0].album.name); 
+		console.log("Song: "+data.tracks.items[0].name); 
+		console.log("preview link : "+data.tracks.items[0].preview_url); 
+		});
+	}
+	 
 }else if (func_call === "movie-this"){
+	if(param.length === 4){
+		var title = param[3];
+		request('http://www.omdbapi.com/?apikey=trilogy&t='+title, function (error, response, body) {
+		  console.log(body); // Print the HTML for the Google homepage.
+		});
+	}else{
+		var title = 'Mr. Nobody';
+		request('http://www.omdbapi.com/?apikey=trilogy&t='+title, function (error, response, body) {
+		  console.log(body); // Print the HTML for the Google homepage.
+		});
+	}
 
 }else if (func_call === "do-what-it-says"){
 
