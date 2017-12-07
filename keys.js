40c6fc40f3da8c49e0f6ce35cd488d3616dde70b
chris-dion <michael.c.dion@gmail.com>
2017-12-07 13:48:12 -0600

    twitter is done

--- keys.js
+++ keys.js
@@ -2,9 +2,9 @@ console.log('this is loaded');
 
 var twitterKeys = {
   consumer_key: 'dNzACdUaDubYAIFmPW3O8O7jk',
-  consumer_secret: '	9ZnaCzKcRzeiaKgs61xE1TIypwZMJHNMYBudp0YiQ7TYoCW3gc',
-  access_token_key: '	938799138195623936-6gXbh5neJqoHmcQPxr1oEhuCLBByv9t',
-  access_token_secret: '9BFrvoP765a5CyUAoKLZtTS0LTQpykTs7iZMjHkCMfRq6',
+  consumer_secret: '9ZnaCzKcRzeiaKgs61xE1TIypwZMJHNMYBudp0YiQ7TYoCW3gc',
+  access_token_key: '938799138195623936-6gXbh5neJqoHmcQPxr1oEhuCLBByv9t',
+  access_token_secret: '9BFrvoP765a5CyUAoKLZtTS0LTQpykTs7iZMjHkCMfRq6'
 }
 
 module.exports = twitterKeys;
--- liri.js
+++ liri.js
@@ -2,11 +2,28 @@
 var keys = require("./keys.js");
 var Twitter = require('twitter');
 
+var client = new Twitter({
+	consumer_key: keys.consumer_key,
+	consumer_secret: keys.consumer_secret,
+	access_token_key: keys.access_token_key,
+	access_token_secret: keys.access_token_secret
+})
+
 var param = process.argv;
 
 var func_call = param [2];
 
 if (func_call === "my-tweets"){
+	var params = {screen_name: 'liri_bot_1'};
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	  if (!error) {
+	  	for (i = 0; i < tweets.length; i++){
+	  		console.log(tweets[i].text);
+	  	}
+	  }else{
+	  	console.log(error);
+	  }
+	});
 
 }else if (func_call === "spotify-this-song"){
 
